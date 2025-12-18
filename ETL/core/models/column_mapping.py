63c9442f5fced975c3996a1623c65ffe7b6e3b2d
tasim313 <mostasimmahmudtasim@gmail.com>
2025-12-18 14:14:34 +0600

    add new model

--- ETL/core/models/column_mapping.py
+++ ETL/core/models/column_mapping.py
@@ -3,20 +3,12 @@ from autoslug import AutoSlugField
 from .base import BaseModel
 from .migration_table import MigrationTable
 
-class ColumnMetaData(BaseModel):
-    table = models.ForeignKey(TableMetaData, on_delete=models.CASCADE, related_name='database_table', null=True, blank=True)
-    column_name = models.CharField(max_length=255, verbose_name = "Database Column Name", related_name="column_name")
-    data_type = models.CharField(max_length=255, verbose_name = "Data Type", related_name="data_type")
-    is_nullable = models.BooleanField(default=False)
-    is_primary_key = models.BooleanField(default=False)
-    is_foreign_key = models.BooleanField(default=False)
-    is_blankable = models.BooleanField(default=False)
-    is_unique = models.BooleanField(default=False)
-    is_indexed = models.BooleanField(default=False)
-    is_auto_created = models.BooleanField(default=False)
-    char_max_length = models.IntegerField(null=True, blank=True)
-    numeric_precision = models.IntegerField(null=True, blank=True)
+class ColumnMapping(BaseModel):
+    migration_table = models.ForeignKey(MigrationTable, on_delete=models.CASCADE, related_name='migration_table', null=True, blank=True)
+    source_column = models.CharField(max_length=255, verbose_name = "Source Column", related_name="source_column")
+    target_column = models.CharField(max_length=255, verbose_name="Target Column", related_name="target_column")
+    is_active = models.BooleanField(default=True)
     slug = AutoSlugField(populate_from='table_name', unique=True)
     
     def __str__(self):
-        return self.table_name
+        return self.source_column + self.target_column
